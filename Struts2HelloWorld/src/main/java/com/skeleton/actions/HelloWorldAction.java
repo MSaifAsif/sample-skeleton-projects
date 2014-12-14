c2d83608df4671e59597403cd0296e30b9d4f7ad
MSaifAsif <saifasifmirza@gmail.com>
2014-12-14 15:24:45 +0500

    Struts2 skeleton war added

---
+++ Struts2HelloWorld/src/main/java/com/skeleton/actions/HelloWorldAction.java
@@ -0,0 +1,17 @@
+package com.skeleton.actions;
+
+import org.apache.log4j.Logger;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorldAction extends ActionSupport {
+
+	private static final long serialVersionUID = 2803230740790812000L;
+	private static final Logger log = Logger.getLogger(HelloWorldAction.class);
+	
+	public String execute(){
+		log.info("Sample log coming from the action class ....");
+		return SUCCESS;
+	}
+
+}
