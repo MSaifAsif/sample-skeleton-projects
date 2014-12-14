c98ae525bc0099ef0b736f28010cc82c11a4eae2
MSaifAsif <saifasifmirza@gmail.com>
2014-12-14 17:21:32 +0500

    Basic JavaDOcs

--- Struts2HelloWorld/src/main/java/com/skeleton/actions/HelloWorldAction.java
+++ Struts2HelloWorld/src/main/java/com/skeleton/actions/HelloWorldAction.java
@@ -4,11 +4,19 @@ import org.apache.log4j.Logger;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+/**
+ * The HelloWorld action class that does nothing but logs and returns "success"
+ * @author saifasif
+ *
+ */
 public class HelloWorldAction extends ActionSupport {
 
 	private static final long serialVersionUID = 2803230740790812000L;
 	private static final Logger log = Logger.getLogger(HelloWorldAction.class);
 	
+	/**
+	 * The overriden method. Should always return a string
+	 */
 	public String execute(){
 		log.info("Sample log coming from the action class ...");
 		return SUCCESS;
