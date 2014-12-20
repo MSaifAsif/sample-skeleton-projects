9209249c4ca7cdbcf879a0efdf68ebb4f414e596
MSaifAsif <saifasifmirza@gmail.com>
2014-12-21 01:28:53 +0500

    Sample test case added

---
+++ UnitTestsCodeCoverage/src/main/java/com/sample/one/SampleOne.java
@@ -0,0 +1,32 @@
+package com.sample.one;
+
+public class SampleOne {
+	
+	public int getSum(int i, int j){
+		if (i == 0) {
+			return j;
+		}
+		else if (j == 0) {
+			return i;
+		}
+		else {
+			return i + j;
+		}
+	}
+	
+	class InnerSample{
+		private int i;
+
+		public InnerSample(int i){
+			this.i = i;
+		}
+		
+		public int getSquare(){
+			if (this.i < 0){
+				return -1;
+			} else {
+				return i*i;
+			}
+		}
+	}
+}
---
+++ UnitTestsCodeCoverage/src/test/java/com/sample/one/SampleOneTest.java
@@ -0,0 +1,40 @@
+package com.sample.one;
+
+import org.junit.Test;
+
+import com.sample.one.SampleOne.InnerSample;
+
+public class SampleOneTest {
+
+	@Test
+	public void testOne(){
+		SampleOne one = new SampleOne();
+		one.getSum(1, 0);
+	}
+
+	@Test
+	public void testTwo(){
+		SampleOne one = new SampleOne();
+		one.getSum(0, 1);
+	}
+
+	@Test
+	public void testThree(){
+		SampleOne one = new SampleOne();
+		one.getSum(0, 0);
+	}
+
+	@Test
+	public void testFour(){
+		SampleOne one = new SampleOne();
+		one.getSum(1, 1);
+	}
+
+	@Test
+	public void testFive(){
+		SampleOne.InnerSample innerSample = new SampleOne.InnerSample(1);
+		innerSample.getSquare();
+	}
+
+
+}
