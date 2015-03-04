9e0c525b8ff17bcb2b88cb73472cf58d56ef342b
MSaifAsif <saifasifmirza@gmail.com>
2015-03-04 23:34:04 +0500

    Docs updated

--- JSFHelloWorld/src/main/java/com/skeleton/beans/MyHelloBean.java
+++ JSFHelloWorld/src/main/java/com/skeleton/beans/MyHelloBean.java
@@ -4,6 +4,16 @@ import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
 
+/**
+ * A {@link ManagedBean} is a normal JavaBean that is contains fields and 
+ * some getter/setters that will be used to display the values on our JSF page
+ * <br>
+ * Values will be referred on the jsf page as myHelloBean#greetMessage and it will 
+ * fetch the values using the correct setter method
+ * 
+ * @author saifasif
+ *
+ */
 @ManagedBean
 public class MyHelloBean implements Serializable {
 
