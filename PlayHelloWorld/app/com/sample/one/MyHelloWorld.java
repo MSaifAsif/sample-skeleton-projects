462910aa33fd200071431f2da7ac2f089c4289df
MSaifAsif <saifasifmirza@gmail.com>
2015-03-14 11:56:38 +0500

    #17 play framework skeleton added and updated

---
+++ PlayHelloWorld/app/com/sample/one/MyHelloWorld.java
@@ -0,0 +1,17 @@
+package com.sample.one;
+
+
+import play.*;
+import play.mvc.*;
+
+import views.html.*;
+
+
+public class MyHelloWorld extends Controller {
+	
+    public static Result greet() {
+        return ok(("Hello World from " + MyHelloWorld.class.getCanonicalName()));
+    }
+
+
+}
