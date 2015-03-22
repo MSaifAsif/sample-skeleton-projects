9fbca9a54eead5d8871aa1fdd9a1af57b06f445b
MSaifAsif <saifasifmirza@gmail.com>
2015-03-22 16:38:23 +0500

    Added 3 other method

--- AxisWebserviceHelloWorld/src/main/java/com/pack/one/Calculator.java
+++ AxisWebserviceHelloWorld/src/main/java/com/pack/one/Calculator.java
@@ -5,10 +5,27 @@ import javax.jws.WebService;
 
 @WebService
 public class Calculator {
-	
+
 	@WebMethod
 	public float sum(float a, float b){
 		return a + b;
 	}
+
+	@WebMethod
+	public float div(float a, float b){
+		if (b == 0.0F) throw new ArithmeticException("Divide by zero not allowed ... Bailing out!");
+		return a / b;
+	}
+
+	@WebMethod
+	public float mul(float a, float b){
+		return a * b;
+	}
+
+	@WebMethod
+	public float sub(float a, float b){
+		return a - b;
+	}
+
 
 }
