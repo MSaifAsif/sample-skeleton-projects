1aac0a2ceff5ace7ae194850a619eeccb6855a5a
MSaifAsif <saifasifmirza@gmail.com>
2015-04-12 19:52:30 +0500

    Comments added with javadocs

--- EJBHelloWorld/src/main/java/com/sample/entity/BeanOneImpl.java
+++ EJBHelloWorld/src/main/java/com/sample/entity/BeanOneImpl.java
@@ -1,7 +1,12 @@
 package com.sample.entity;
 
+import javax.ejb.SessionBean;
 import javax.ejb.Stateless;
-
+/**
+ * This is the implementation of a {@link Stateless} {@link SessionBean}
+ * @author saifasif
+ *
+ */
 @Stateless
 public class BeanOneImpl implements IBeanOne {
 
