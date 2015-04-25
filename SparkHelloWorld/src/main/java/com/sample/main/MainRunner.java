1e5c68cadced06b29fe80891000aba31ec3e7af7
MSaifAsif <saifasifmirza@gmail.com>
2015-04-26 00:47:28 +0500

    Support enhanced for spark

--- SparkHelloWorld/src/main/java/com/sample/main/MainRunner.java
+++ SparkHelloWorld/src/main/java/com/sample/main/MainRunner.java
@@ -13,18 +13,25 @@ public class MainRunner {
 	private static final Logger log = Logger.getLogger(MainRunner.class);
 
 	public static void main(String[] args) {
-		get("/helloWorld", new Route() {
 
+		log.info("INit server ...");
+		
+		get("/helloWorld", new Route() {
 			@Override
 			public Object handle(Request arg0, Response arg1) throws Exception {
 				log.info("Init get .... ");
-				return "Get done";
+				return "This is from the Get method. Hello world";
 			}
 		});
 
 		post("/post1", (req, res) -> {
 			log.info("Init post ....");
 			return "Getting from post";
+		});
+
+		get("/helloWorld2", (req, res) -> {
+			log.info(req.queryParams());
+			return "";
 		});
 
 	}
