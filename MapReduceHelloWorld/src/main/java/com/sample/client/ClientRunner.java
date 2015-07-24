e4daf079b56f5fb4e98d21b03d7c1dcfe22a7fbd
MSaifAsif <saifasifmirza@gmail.com>
2015-07-24 16:20:32 +0100

    Support for MR added

---
+++ MapReduceHelloWorld/src/main/java/com/sample/client/ClientRunner.java
@@ -0,0 +1,47 @@
+package com.sample.client;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import com.sample.one.mappers.WordCountMapper;
+import com.sample.one.reducers.WordCountReducer;
+
+public class ClientRunner extends Configured implements Tool {
+
+    @Override
+    public int run(String[] args) throws Exception {
+        Job job = new Job(getConf());
+        job.setJarByClass(getClass());
+        job.setJobName(getClass().getSimpleName());
+
+        // the input file name on the HDFS
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        // the output file on the hdfs
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        // Set the mapper name
+        job.setMapperClass(WordCountMapper.class);
+        
+        // Set the combiner and the reducer classes
+        job.setCombinerClass(WordCountReducer.class);
+        job.setReducerClass(WordCountReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+
+        // should we wait for job completion or not
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int rc = ToolRunner.run(new ClientRunner(), args);
+        System.exit(rc);
+    }
+}
---
+++ MapReduceHelloWorld/src/main/java/com/sample/one/mappers/WordCountMapper.java
@@ -0,0 +1,30 @@
+package com.sample.one.mappers;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+/**
+ * The {@link Mapper} class.
+ * This will read the input file and map keys with values
+ * 
+ * @author saifasif
+ *
+ */
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, LongWritable>{
+    
+    private Text word = new Text();
+    private LongWritable count = new LongWritable();
+    
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
+        
+        String[] split = value.toString().split("\t+");
+        word.set(split[0]);
+        if (split.length > 2){
+            count.set(Long.parseLong(split[2]));
+            context.write(word, count);
+        }
+    }
+}
