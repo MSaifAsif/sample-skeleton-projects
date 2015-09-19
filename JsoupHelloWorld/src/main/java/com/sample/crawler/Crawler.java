e188f3f680a53986cdf0118a1be854f71b0b5ea4
MSaifAsif <saifasifmirza@gmail.com>
2015-09-19 16:06:10 +0500

    Crawler with local cache implemente

--- JsoupHelloWorld/src/main/java/com/sample/crawler/Crawler.java
+++ JsoupHelloWorld/src/main/java/com/sample/crawler/Crawler.java
@@ -21,17 +21,26 @@ public class Crawler {
     public static void processPage(String URL) throws SQLException, IOException{
         //check if the given URL is already in database
         //get useful information
-        Document doc = Jsoup.connect(URL).get();
-
-        if(doc.text().contains("research")){
-            System.out.println(URL);
+        if (ContentList.isContentInMap(URL)){
+            return;
+        }
+        Document doc = null;
+        try{
+            doc = Jsoup.connect(URL).timeout(5000).get();
+            if(doc.text().contains("research")){
+                System.out.println(URL);
+                ContentList.insertKey(URL, URL);
+            }
+            
+            //get all links and recursively call the processPage method
+            Elements questions = doc.select("a[href]");
+            for(Element link: questions){
+                if(link.attr("href").contains("mit.edu"))
+                    processPage(link.attr("abs:href"));
+            }
+        } catch (Exception e){
+            System.out.println("skipping .... " + URL);
         }
 
-        //get all links and recursively call the processPage method
-        Elements questions = doc.select("a[href]");
-        for(Element link: questions){
-            if(link.attr("href").contains("mit.edu"))
-                processPage(link.attr("abs:href"));
-        }
     }
 }
