413815c0b92b83c2e9374b487536670bcbf025f2
MSaifAsif <saifasifmirza@gmail.com>
2015-09-24 13:06:19 +0500

    Improved grammer

--- SpringBootHelloWorld/src/main/java/com/sample/runners/StandAloneClient.java
+++ SpringBootHelloWorld/src/main/java/com/sample/runners/StandAloneClient.java
@@ -7,14 +7,19 @@ import com.sample.services.MyRestServicesController;
 
 /**
  * Standalone runner. Will be responsible for exposing the web services in an embedded jetty 
- * server. <br><br>This will serve as the entry point of the application when<br> <code>[~]$ mvn spring-boot:run</code>
+ * server. <br><br>This will serve as the entry point of the application when the command<br> <code>[~]$ mvn spring-boot:run</code>
+ * will be executed
+ * 
  * @author saifasif
  *
  */
 public class StandAloneClient {
-    
+
     public static void main(String[] args) {
+        // I need some logging here
         BasicConfigurator.configure();
+
+        // Fire the controller
         SpringApplication.run(MyRestServicesController.class, args);
     }
 
