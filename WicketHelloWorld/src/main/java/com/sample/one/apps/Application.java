7a1dcaa740aed9492e4b93492a521df1bc91c64f
MSaifAsif <saifasifmirza@gmail.com>
2015-10-25 01:48:36 +0500

    Support for apache wickets

---
+++ WicketHelloWorld/src/main/java/com/sample/one/apps/Application.java
@@ -0,0 +1,20 @@
+package com.sample.one.apps;
+
+import org.apache.wicket.Page;
+import org.apache.wicket.protocol.http.WebApplication;
+
+import com.sample.one.pages.GreetingsPage;
+
+/**
+ * Entry point of the application.
+ * @author saifasif
+ *
+ */
+public class Application extends WebApplication {
+
+    @Override
+    public Class<? extends Page> getHomePage() {
+        return GreetingsPage.class;
+    }
+
+}
