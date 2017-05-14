d5f155548fdd23e687a4f2deb6039df42dcd2504
MSaifAsif <saifasifmirza@gmail.com>
2017-05-14 15:51:21 +0200

    reenabled integration tests

--- RESTEasy/src/main/java/com/sample/one/GreetRestService.java
+++ RESTEasy/src/main/java/com/sample/one/GreetRestService.java
@@ -1,16 +1,15 @@
 package com.sample.one;
 
+import com.sample.utilities.MyBean;
+import com.sample.utilities.TransformationUtils;
+import org.apache.log4j.Logger;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
-
-import org.apache.log4j.Logger;
-
-import com.sample.utilities.MyBean;
-import com.sample.utilities.TransformationUtils;
 
 /**
  * The actual rest service that will be exposed to the user with /greet in the URL
@@ -39,7 +38,7 @@ public class GreetRestService extends Application {
     /**
      * A sample rest method exposed at URL '/greet/get_sample/{caller}'
      *
-     * @param name A sample String text
+     * @param caller A sample String text
      * @return {@link String} json format string that will be displayed on the page
      */
     @GET
