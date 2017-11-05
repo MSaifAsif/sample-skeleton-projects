3127276177ae50efa57e97ae55944453c0ff18b7
MSaifAsif <saifasifmirza@gmail.com>
2017-11-05 18:31:28 +0100

    Ebean sample added

--- Ebean/src/test/java/com/test/human/CrudTest.java
+++ Ebean/src/test/java/com/test/human/CrudTest.java
@@ -3,6 +3,8 @@ package com.test.human;
 import com.ebean.model.Human;
 import io.ebean.Ebean;
 import io.ebean.EbeanServer;
+import io.ebean.Query;
+
 import org.apache.log4j.BasicConfigurator;
 import org.junit.Test;
 
@@ -17,18 +19,26 @@ public class CrudTest {
     @Test
     public void insert_via_server() {
         BasicConfigurator.configure();
-        Human being;
+        Human being = null;
 
         being = new Human();
         being.setAge(12);
         being.setFirstName("I");
         being.setLastName("be human");
+        being.setUserId(1L);
         being.setGender("m");
 
         EbeanServer server = Ebean.getDefaultServer();
-        server.save(being);
+        server.beginTransaction();
 
-        assertNotNull(being.getUserId());
+        server.save(being);
+        server.commitTransaction();
+
+        assertNotNull(being.getAge());
+        
+        Query<Human> find = server.find(Human.class);
+        Human findOne = find.findOne();
+        System.out.println(findOne.getAge());
     }
 
 }
