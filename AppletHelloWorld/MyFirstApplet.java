b60d0e126f2d10e532e912e5b93702bd31591dd7
M Saif Asif <6280554+MSaifAsif@users.noreply.github.com>
2019-10-30 22:48:02 +0100

    Update MyFirstApplet.java

--- AppletHelloWorld/MyFirstApplet.java
+++ AppletHelloWorld/MyFirstApplet.java
@@ -3,7 +3,7 @@ import java.awt.Graphics;
 
 public class MyFirstApplet extends Applet {
 
-   private static final long serialVersionUID = -3783584337636720597L;
+    private static final long serialVersionUID = -3783584337636720597L;
   
     /**
      * The init method will be called when the applet is launched
@@ -24,10 +24,10 @@ public class MyFirstApplet extends Applet {
    }
   public void paint(Graphics g){
          str=str+"paint-";;
-g.drawString(str,40,100);
-g.drawString("Hello from the Applet.", 40,40);
-g.drawString("How are you doing?", 40, 60);
-g.drawString("We wish you a pleasant day today.", 40, 80);
+         g.drawString(str,40,100);
+         g.drawString("Hello from the Applet.", 40,40);
+         g.drawString("How are you doing?", 40, 60);
+         g.drawString("We wish you a pleasant day today.", 40, 80);
     }
 
 }
