ef0701d9c3891d2831790ef1b1957de891f67158
MSaifAsif <saifasifmirza@gmail.com>
2020-09-25 12:32:20 +0200

    Added logback skeleton

---
+++ logging/logback/src/main/java/com/logback/sample/Main.java
@@ -0,0 +1,15 @@
+package com.logback.sample;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("This message is coming from the log");
+    }
+
+
+}
